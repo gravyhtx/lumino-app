c2f019a4c642ceb6a574cb67bda1b7fde77fedad
gravyhtx <godisgravy@gmail.com>
2025-01-28 13:11:59 -0600

    New Windows

--- src/components/layout/window/popup-window.tsx
+++ src/components/layout/window/popup-window.tsx
@@ -31,7 +31,7 @@ export function PopupWindow({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/80 backdrop-blur-sm" onClick={onClose}>
-      <div className={`max-w-${columns?'6':''}xl w-full p-4`}>
+      <div className={`max-w-${columns?'6':''}xl p-4`}>
         <div className="rounded-lg border bg-card text-card-foreground shadow-lg">
           {/* Header Section */}
           <div className="flex items-center justify-between border-b p-3">
