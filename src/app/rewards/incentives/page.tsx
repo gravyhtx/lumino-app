e0a9a01e6f97672e9400afbbf216cdc51300220b
phenomenaughty <phenomenaughty@gmail.com>
2025-02-17 03:57:54 -0600

    Dark styles

--- src/app/rewards/incentives/page.tsx
+++ src/app/rewards/incentives/page.tsx
@@ -2,13 +2,11 @@
 
 import React, { useEffect, useState } from 'react';
 import { SideNav } from '@/components/layout/window/side-nav';
-import PointsSummary from '@/components/layout/rewards/points';
 import { ViewContainer } from '@/components/layout/views/view-container';
 import { Header } from '@/components/layout/window/header';
 import { Home, LineChart, Package, ShoppingCart } from 'lucide-react';
 import BouncingDotsLoader from '@/components/layout/loading/BouncingDots';
-import BadgesAndAchievements from '@/components/layout/rewards/badges';
-import RewardRedemption from '@/components/layout/rewards/redeem';
+import IncentivesHub from '@/components/layout/rewards/incentives-hub';
 
 export default function Dashboard() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -30,7 +28,7 @@ export default function Dashboard() {
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 w-full overflow-auto">
       { isLoaded ?
           <ViewContainer>
-            <RewardRedemption />
+            <IncentivesHub />
           </ViewContainer>
         :
         <div className="flex items-center">
